685b07181cba2d8759721da76ffc692e7e7a2dd0
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-16 00:07:46 -0600

    added the function to the supervisor js that will allow the user to add a new department

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -22,7 +22,7 @@ function startQuestions() {
       message: "What would you like to do?",
       choices: [
           "View Product Sales by Department",
-          "View Low Inventory"]
+          "Create New Department"]
       })
     .then(function(answer) {
       if (answer.action === "View Product Sales by Department") {
@@ -30,14 +30,48 @@ function startQuestions() {
       }
 
       else {
-
+        newDepartment();
       }
     });
 }
 
 function viewProducts() {
-  var query = "SELECT department_id, department_name, over_head_costs, product_sales, over_head_costs - products_sales AS total_profit FROM products LEFT JOIN departments ON products.department_name = departments.department_name GROUP BY department_name";
+  var query = "SELECT department_id, department_name, over_head_costs, product_sales, over_head_costs - products_sales AS total_profit FROM departments LEFT JOIN products ON products.department_name = departments.department_name GROUP BY department_name";
   connection.query(query, function(err, res) {
       //SHOW table
   });
+}
+
+function newDepartment(){
+  inquirer
+  .prompt([
+      {
+        type: "input",
+        message: "What is the name of the new department?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is the over head cost of the new department?",
+        name: "cost"
+      }
+    ])
+  .then(function(answer) {
+    var query = "INSERT INTO departments SET ?";
+    connection.query(query, 
+      {
+        department_name: answer.name, 
+        over_head_cost: answer.cost 
+      }, 
+      function(err, res) {
+          console.log(
+              "You have added the department: " +
+                answer.name +
+                " to the store. With the over head cost of $" +
+                answer.cost
+            );        
+          });
+      startQuestions();
+  });
+
 }
