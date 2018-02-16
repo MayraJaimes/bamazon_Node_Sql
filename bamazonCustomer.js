7996014d863eb725c027ae8d23f74b099336f866
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-15 22:08:05 -0600

    added the if and else statement after the inquirer. If enough quantity it will udpate database and show user price. else will display message stating not enough items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,53 +15,57 @@ startQuestions();
 });
 
 function startQuestions() {
-    
-    var query = "SELECT item_id,product_name,price FROM bamazon";
-    connection.query(query, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Item ID: " +
-              res[i].item_id +
-              " || Product Name: " +
-              res[i].product_name +
-              " || Price: " +
-              res[i].price
-          );
-        }
-    });
 
-    inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the ID of the product that you would like to buy?",
-        name: "id"
-      },
-      {
-        type: "input",
-        message: "How many units of the product would you like to buy?",
-        name: "units"
-      }
-    ])
-    .then(function(answer) {
-        answer.id;
-        answer.units;
+  var query = "SELECT item_id,product_name,price FROM bamazon";
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "Item ID: " +
+        res[i].item_id +
+        " || Product Name: " +
+        res[i].product_name +
+        " || Price: " +
+        res[i].price
+      );
+    }
+  });
 
-        //Once order is placed, check if the store has enough quantity to meet the customer's request
-          //Get the item information
-          //Save item stock_quantity number in a variable
-          
-          //if(stock_quantity >= answer.units ){
-          //   complete the order
-          //      update database stock.quantity - answer.units
-          //      multiply price * answer.units
-          // }   
-          
-          // else {
-          //   console.log("Insufficient quantity!");
-          // }
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the ID of the product that you would like to buy?",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "How many units of the product would you like to buy?",
+      name: "units"
+    }
+  ])
+  .then(function(answer) {
+    answer.id;
+    answer.units;
+
+    var query_two = "SELECT price,stock_quantity FROM bamazon WHERE ?";
+    connection.query(query_two, { item_id: answer.id }, function(err, res) {
         
-    });
-}
-  
+      var productPrice = res[0].price;
+      var productStockQuant = res[0].stock_quantity;
 
+      if(productStockQuant >= answer.units){
+        var newProductQuant = productStockQuant - answer.units;
+        var totalPrice = productPrice * answer.units;
+
+        var query_three = "UPDATE products SET ? WHERE ?";
+        connection.query(query_three, [{stock_quantity: newProductQuant},{item_id: answer.id}], function(err, res) {
+          console.log ("Order completed your total comes out to: $" + totalPrice);
+        });
+      }   
+      
+      else {
+        console.log("Insufficient quantity!");
+      }
+    });
+  });
+}
