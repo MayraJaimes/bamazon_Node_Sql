a20faae7abdef0b828ec99607b8403afe195d09d
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-15 21:10:03 -0600

    Added pseudocode for the functions that will be available after the user inputs the answer to the questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,10 +16,11 @@ startQuestions();
 
 function startQuestions() {
 
-  // function displayProducts () {
+  function displayProducts () {
   //   first going to display function of all of the items available for sale (including; the ids, names, and prices of products for sale) 
-  // }
-  
+  }
+
+//Then prompt users with two messages:
     inquirer
     .prompt([
       {
@@ -34,9 +35,22 @@ function startQuestions() {
       }
     ])
     .then(function(answer) {
-
         answer.id;
         answer.units;
+
+        //Once order is placed, check if the store has enough quantity to meet the customer's request
+          //Get the item information
+          //Save item stock_quantity number in a variable
+          
+          //if(stock_quantity >= answer.units ){
+          //   complete the order
+          //      update database stock.quantity - answer.units
+          //      multiply price * answer.units
+          // }   
+          
+          // else {
+          //   console.log("Insufficient quantity!");
+          // }
         
     });
 }
